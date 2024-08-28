1792ab990b99f375df9f930b7a3802eb6a46bcd7
Sam Sesh <admin@samsesh.net>
2024-08-28 19:50:04 +0330

    add fixer

---
+++ fixer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# URL of the new repository file
+URL="https://raw.githubusercontent.com/samsesh/centos7fixrepo/Localhost/CentOS-Base.repo"
+
+# Path to the local repository file
+LOCAL_FILE="/etc/yum.repos.d/CentOS-Base.repo"
+
+# Download the new repository file
+echo "Downloading the new repository file from $URL..."
+curl -o "$LOCAL_FILE" "$URL"
+
+# Check if the download was successful
+if [ $? -eq 0 ]; then
+    echo "File downloaded and replaced successfully."
+else
+    echo "Error occurred while downloading the new file."
+    exit 1
+fi
+
+# Update the YUM repositories
+echo "Updating YUM repositories..."
+yum clean all
+yum update -y
+
+echo "Repository update completed successfully."
+
